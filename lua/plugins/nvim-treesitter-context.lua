98779e22db4e6dd1c1400c7a348fb86b47afd4ae
Fernando NUNEZ <fernando.nunez@ampere.cars>
2025-06-04 17:57:31 +0200

    auto activate treesitter context

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -1,3 +1,11 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
+
+	config = function()
+		require("treesitter-context").setup({
+			enable = true, -- this MUST be true
+			max_lines = 5, -- how many lines the context window can span
+			mode = "cursor", -- or "topline"
+		})
+	end,
 }
