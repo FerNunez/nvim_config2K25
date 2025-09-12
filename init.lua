ced32fb7635af2bf7e77d73583c19ff95b2fa6d7
Fernando NUNEZ <fernunezmendoza@gmail.com>
2025-09-12 18:57:46 +0200

    add keymaps fpr copy functions names and file paths

--- init.lua
+++ init.lua
@@ -18,3 +18,39 @@ function EditFromLazygit(file_path)
 		vim.cmd("e " .. vim.fn.fnameescape(file_path))
 	end
 end
+
+-- Custom command: Substitue first Word
+-- :'<,'>s/\(\S\+\)\s\+.*/\1/
+vim.api.nvim_create_user_command("SubFirstWord", function()
+	vim.cmd("'<,'>s/\\(\\S\\+\\)\\s\\+.*/\\1/")
+end, { range = true })
+-- Key mapping
+vim.keymap.set("v", "<leader>sf", ":<C-u>'<,'>s/\\(\\S\\+\\)\\s\\+.*/\\1/<CR>", { desc = "Keep only first word" })
+
+-- Mapping to copyt relative path
+vim.keymap.set("n", "<leader>cp", function()
+	vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Copy relative file path" })
+vim.keymap.set("n", "<leader>cP", function()
+	vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Copy absolute file path" })
+
+vim.keymap.set("n", "<leader>cf", function()
+	local ts_utils = require("nvim-treesitter.ts_utils")
+	local node = ts_utils.get_node_at_cursor()
+	local function_nodes = { "function_declaration", "method_declaration" }
+
+	while node do
+		if vim.tbl_contains(function_nodes, node:type()) then
+			local name_node = node:field("name")[1]
+			if name_node then
+				local fn_name = vim.treesitter.get_node_text(name_node, 0)
+				vim.fn.setreg("+", fn_name)
+				print("Copied function name: " .. fn_name)
+				return
+			end
+		end
+		node = node:parent()
+	end
+	print("No function found at cursor")
+end, { desc = "Copy Go function name under cursor" })
